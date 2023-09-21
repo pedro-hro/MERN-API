60ef5e131d4926242c1e2dd034902d0be9113872
pedrohenriquereisdeoliveira@gmail.com <pedro@apx_managed_dnf.vanilla>
2023-09-20 21:54:19 -0300

    refactor: Added project architecture

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
+const userRoute = require ("./src/routes/user.route");
 
-app.listen(3000)
+app.use("/soma", userRoute);
 
-app.get("/", (req, res) => {
-        res.send("Hello World!")
-})
+app.listen(3000);
