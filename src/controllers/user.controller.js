0e7678518bb9d06ad76b6c99a22ee6f8d8423234
pedrohenriquereisdeoliveira@gmail.com <pedro@apx_managed_dnf.vanilla>
2023-09-22 18:54:31 -0300

    refactor/fix: New GET user route, update, and fix crashes in the create controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,25 +6,25 @@ const create = async (req, res) => {
 
   if (!name || !username || !email || !password || !avatar || !background) {
     res.status(400).send({ message: "Submit all fields" });
+  } else {
+    const user = await userService.createService(req.body);
+
+    if (!user) {
+      return res.status(400).send({ message: "Error creating user." });
+    }
+
+    res.status(201).send({
+      message: "User created!",
+      user: {
+        id: user._id,
+        name,
+        username,
+        email,
+        avatar,
+        background,
+      },
+    });
   }
-
-  const user = await userService.createService(req.body);
-
-  if (!user) {
-    return res.status(400).send({ message: "Error creating user." });
-  }
-
-  res.status(201).send({
-    message: "User created!",
-    user: {
-      id: user._id,
-      name,
-      username,
-      email,
-      avatar,
-      background,
-    },
-  });
 };
 
 const findAll = async (req, res) => {
@@ -52,4 +52,35 @@ const findById = async (req, res) => {
   res.send(user);
 };
 
-module.exports = { create, findAll, findById };
+const update = async (req, res) => {
+  const { name, username, email, password, avatar, background } = req.body;
+
+  if (!name && !username && !email && !password && !avatar && !background) {
+    res.status(400).send({ message: "Submit at least one field." });
+  } else {
+    const id = req.params.id;
+
+    if (!mongooose.Types.ObjectId.isValid(id)) {
+      return res.status(400).send({ message: "Invalid ID." });
+    } else {
+      const user = await userService.findByIdService(id);
+
+      if (!user) {
+        return res.status(404).send({ message: "User not found." });
+      } else {
+        await userService.updateService (
+          id,
+          name,
+          username,
+          email,
+          password,
+          avatar,
+          background
+        );
+          res.status(200).send({message: "User successfully updated."})
+      }
+    }
+  }
+};
+
+module.exports = { create, findAll, findById, update };
