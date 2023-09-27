9ed1a1c68ec684abe6dfb6ceb4d736232d0f641c
pedrohenriquereisdeoliveira@gmail.com <pedro@apx_managed_dnf.vanilla>
2023-09-27 18:38:01 -0300

    refactor: Added server-side pagination for the findAll News function.

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -1,4 +1,4 @@
-import { createService, findAllService } from "../services/news.service.js";
+import { createService, findAllService, countNews } from "../services/news.service.js";
 
 const create = async (req, res) => {
   try {
@@ -22,13 +22,43 @@ const create = async (req, res) => {
 
 const findAll = async (req, res) => {
   try {
-    const news = await findAllService();
+    let { limit, offset } = req.query;
+    limit = Number(limit || 5);
+    offset = Number(offset || 0);
 
-    if (news.lenght === 0) {
+    const news = await findAllService(offset, limit);
+    const total = await countNews();
+
+    const currentUrl = req.baseUrl;
+
+    const next = offset + limit;
+    const nextUrl = next < total ? `${currentUrl}?limit=${limit}&offset=${next}` : null;
+
+    const previous = offset - limit < 0 ? null : offset - limit;
+    const previousUrl = previous != null ? `${currentUrl}?limit=${limit}&offset=${previous}` : null;
+
+    if (news.length === 0) {
       return res.status(400).send({ message: "There are no created news." });
     }
 
-    res.send(news);
+    res.send({
+      nextUrl,
+      previousUrl,
+      limit,
+      offset,
+      total,
+      results: news.map((newsItem) => ({
+        id: newsItem._id,
+        title: newsItem.title,
+        text: newsItem.text,
+        banner: newsItem.banner,
+        likes: newsItem.likes,
+        comments: newsItem.comments,
+        name: newsItem.user.name,
+        username: newsItem.user.username,
+        userAvatar: newsItem.user.avatar,
+      })),
+    });
   } catch (err) {
     res.status(500).send({ message: err.message });
   }
